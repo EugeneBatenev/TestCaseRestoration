538c76a5e6faaf4dafc14df531ca28387fcf59b3
Evgeniy Batenev <73171575+EugeneBatenev@users.noreply.github.com>
2024-10-01 16:17:08 +0200

    Update TestCaseRestoration.sh
    Added counter of found test cases
    Added user confirmation before restoring test cases

--- TestCaseRestoration.sh
+++ TestCaseRestoration.sh
@@ -3,7 +3,7 @@
 # Переменные для API эндпоинта и пользователя
 export ENDPOINT="https://testing.testops.cloud"
 export USER_TOKEN="b09e811d-7f74-4425-a67e-d19a2a390c0f"
-ALLURE_PROJECT_ID=<PROJECT_ID>
+ALLURE_PROJECT_ID=10183
 RESTORE_TEST_CASES_PER_RUN=100
 
 # Получение Bearer-токена
@@ -24,9 +24,12 @@ fi
 
 echo "Bearer token obtained successfully: $JWT_TOKEN"
 
+# URL для получения всех удалённых тест-кейсов
+FILTER_URL="${ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}"
+
 # Получение удалённых тест-кейсов
 echo "Fetching deleted test cases..."
-RESULT=$(curl -s -X GET "${ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}" \
+RESULT=$(curl -s -X GET "${FILTER_URL}" \
   --header "accept: */*" \
   --header "Authorization: Bearer ${JWT_TOKEN}")
 
@@ -36,26 +39,42 @@ if [ -z "$RESULT" ]; then
   exit 1
 fi
 
-echo "Found deleted test cases in project ${ALLURE_PROJECT_ID}:"
+# Подсчёт количества удалённых тест-кейсов
+DELETED_COUNT=$(echo $RESULT | jq '.content | length')
 
-# Извлечение ID тест-кейсов
-IDS=$(echo $RESULT | jq .content[].id)
+echo "Found ${DELETED_COUNT} deleted test case(s) in project ${ALLURE_PROJECT_ID}:"
 
-# Проверка, есть ли ID для восстановления
-if [ -z "$IDS" ]; then
+# Если тест-кейсы не найдены, завершение работы скрипта
+if [ "$DELETED_COUNT" -eq 0 ]; then
   echo "No test cases to restore."
   exit 0
 fi
 
-# Восстановление каждого тест-кейса
-for ID in ${IDS}; do
-    echo "Restoring test case ID: ${ID}"
-    curl -s -X PATCH "${ENDPOINT}/api/rs/testcase/${ID}" \
-      --header "accept: */*" \
-      --header "Content-Type: application/json" \
-      --header "Authorization: Bearer ${JWT_TOKEN}" \
-      -d "{\"deleted\": false}"
-    echo "Test case ${ID} restored."
-done
+# Подтверждение пользователя для восстановления тест-кейсов
+read -p "Do you want to restore the deleted test cases? (y/n): " CONFIRMATION
 
-echo "Restoration process completed."
+if [ "$CONFIRMATION" == "y" ]; then
+  # Извлечение ID тест-кейсов
+  FILTERED_IDS=$(echo $RESULT | jq .content[].id)
+
+  # Восстановление каждого тест-кейса
+  for ID in ${FILTERED_IDS}; do
+      echo "Restoring test case ID: ${ID}"
+      curl -s -X PATCH "${ENDPOINT}/api/rs/testcase/${ID}" \
+        --header "accept: */*" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Bearer ${JWT_TOKEN}" \
+        -d "{\"deleted\": false}"
+      echo "Test case ${ID} restored."
+  done
+
+  echo "Restoration process completed."
+
+elif [ "$CONFIRMATION" == "n" ]; then
+  echo "Operation canceled by user. Exiting in 10 seconds..."
+  sleep 10
+  exit 0
+else
+  echo "Invalid input. Please run the script again and enter 'y' or 'n'."
+  exit 1
+fi
