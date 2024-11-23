4791070e270d3bc1fc3422348bf6a0f8a2a93f29
Evgeniy Batenev <73171575+EugeneBatenev@users.noreply.github.com>
2024-11-23 19:59:36 +0100

    Update TestCaseRestoration.sh

--- TestCaseRestoration.sh
+++ TestCaseRestoration.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Функция для кодирования URL
+urlencode() {
+    local data
+    data=$(echo "$1" | jq -sRr @uri)
+    echo "$data"
+}
+
 # Переменные для API эндпоинта и пользователя
-ENDPOINT="https://your_instance.com"
-USER_TOKEN="allure-token"
-ALLURE_PROJECT_ID=your_project_id
+export ENDPOINT="https://testing.testops.cloud"
+export USER_TOKEN="b09e811d-7f74-4425-a67e-d19a2a390c0f"
+ALLURE_PROJECT_ID=10183
 RESTORE_TEST_CASES_PER_RUN=100
+AQL_QUERY=''  # AQL выражение для фильтрации тест-кейсов
 
 # Получение Bearer-токена
 echo "Obtaining Bearer token..."
@@ -24,8 +32,17 @@ fi
 
 echo "Bearer token obtained successfully: $JWT_TOKEN"
 
-# URL для получения всех удалённых тест-кейсов
-FILTER_URL="${ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}"
+# Если указан AQL запрос, кодируем его и добавляем в URL
+if [ -n "$AQL_QUERY" ]; then
+  ENCODED_AQL_QUERY=$(urlencode "$AQL_QUERY")
+  echo "Original AQL query: $AQL_QUERY"
+  echo "Encoded AQL query for URL: $ENCODED_AQL_QUERY"
+  
+  FILTER_URL="${ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}&filter=${ENCODED_AQL_QUERY}"
+  echo "Using AQL query: $AQL_QUERY"
+else
+  FILTER_URL="${ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}"
+fi
 
 # Получение удалённых тест-кейсов
 echo "Fetching deleted test cases..."
@@ -44,37 +61,23 @@ DELETED_COUNT=$(echo $RESULT | jq '.content | length')
 
 echo "Found ${DELETED_COUNT} deleted test case(s) in project ${ALLURE_PROJECT_ID}:"
 
-# Если тест-кейсы не найдены, завершение работы скрипта
-if [ "$DELETED_COUNT" -eq 0 ]; then
+# Извлечение ID тест-кейсов
+FILTERED_IDS=$(echo $RESULT | jq .content[].id)
+
+if [ -z "$FILTERED_IDS" ]; then
   echo "No test cases to restore."
   exit 0
 fi
 
-# Подтверждение пользователя для восстановления тест-кейсов
-read -p "Do you want to restore the deleted test cases? (y/n): " CONFIRMATION
+# Восстановление каждого тест-кейса
+for ID in ${FILTERED_IDS}; do
+    echo "Restoring test case ID: ${ID}"
+    curl -s -X PATCH "${ENDPOINT}/api/rs/testcase/${ID}" \
+      --header "accept: */*" \
+      --header "Content-Type: application/json" \
+      --header "Authorization: Bearer ${JWT_TOKEN}" \
+      -d "{\"deleted\": false}"
+    echo "Test case ${ID} restored."
+done
 
-if [ "$CONFIRMATION" == "y" ]; then
-  # Извлечение ID тест-кейсов
-  FILTERED_IDS=$(echo $RESULT | jq .content[].id)
-
-  # Восстановление каждого тест-кейса
-  for ID in ${FILTERED_IDS}; do
-      echo "Restoring test case ID: ${ID}"
-      curl -s -X PATCH "${ENDPOINT}/api/rs/testcase/${ID}" \
-        --header "accept: */*" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Bearer ${JWT_TOKEN}" \
-        -d "{\"deleted\": false}"
-      echo "Test case ${ID} restored."
-  done
-
-  echo "Restoration process completed."
-
-elif [ "$CONFIRMATION" == "n" ]; then
-  echo "Operation canceled by user. Exiting in 10 seconds..."
-  sleep 10
-  exit 0
-else
-  echo "Invalid input. Please run the script again and enter 'y' or 'n'."
-  exit 1
-fi
+echo "Restoration process completed."
